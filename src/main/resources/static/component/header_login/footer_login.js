7e722b6537ee9fb1465f41a129db69f0795d8b64
YakumoTsuji <956298096@qq.com>
2021-02-02 12:07:13 +0800

    init

---
+++ src/main/resources/static/component/header_login/footer_login.js
@@ -0,0 +1,18 @@
+Vue.component('footer_login',{
+    template: `
+        <div class="footer">
+            <div class="wrrant_area">
+                <p style="text-align: center;line-height: 20px;height: 20px;">
+                    <a>用户协议</a>
+                    <em class="footer_em">|</em>
+                    <a>关于账号和隐私的声明</a>
+                    <em class="footer_em">|</em>
+                    <a>常见问题</a>
+                </p>
+                <p style="    line-height: 12px;height: 12px;margin-top: 10px;color: #000;opacity: 0.3;text-align: center;">
+                    第八组 版权所有 © 2021-2031
+                </p>
+            </div>
+        </div>
+	`,
+})
---
+++ src/main/resources/static/component/header_login/header_login.js
@@ -0,0 +1,15 @@
+Vue.component('header_login',{
+    template: `
+        <div class="login_header" >
+            <div class="header_center">
+                <div  class="mainlogo">
+                    <img src="image/main_logo.jpg">
+                </div>
+                <div class="websiteName">
+                    <div class="split_line"></div>        
+                    <div class="HeadTitle">第八组商城</div>        
+                </div>
+            </div>
+        </div>
+	`,
+})
